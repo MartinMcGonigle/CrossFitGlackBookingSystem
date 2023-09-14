19419e98d7f7e109dbf751b0045be136deef9ea6
Martin McGonigle <martin_mcgonigle@outlook.com>
2023-09-14 20:38:20 +0100

    Project setup.

---
+++ CrossFit.Glack/CrossFit.Glack.Staff/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CrossFit.Glack.Staff.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
